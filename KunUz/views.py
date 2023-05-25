93a25b4878d6be3a486fcce63b9f29881a3f997d
Bakha022 <abduqahhorovbahodir536@gmail.com>
2023-05-25 21:00:47 +0500

    war section qoshildi ammo logika xato:detailview yozishda oldingi lokiga tori kemasligi mumkin!

--- KunUz/views.py
+++ KunUz/views.py
@@ -13,14 +13,28 @@ class CategoryView(ListView):
 def RegionalView(request):
     object_list = NewsType.objects.all()
     one_article = RegionalNews.objects.first()
-    four_articels = RegionalNews.objects.order_by('id')[:4]
+    four_articels = RegionalNews.objects.order_by('?')[:4]
     theEditor = RegionalNews.objects.order_by('id')[4:7]
+    newNews = RegionalNews.objects.order_by("-id")[:8]
+    currentNews = RegionalNews.objects.filter(newsTypes='dolzarb').first()
+    singleCurrent = RegionalNews.objects.filter(newsTypes='dolzarb')[1:]
+    interview = RegionalNews.objects.filter(newsTypes='intervyu')
+    warNews_1 = RegionalNews.objects.filter(newsTypes='rossiyaukraine').first()
+    warNews = RegionalNews.objects.filter(newsTypes='rossiyaukraine')[1:5]
+    
     ctx = {
         "four_articels" : four_articels,
         "one_articles": one_article,
         "object_list": object_list,
-        "theEditor": theEditor
+        "theEditor": theEditor,
+        "newNews": newNews,
+        "currentNews": currentNews,
+        "singleCurrent" : singleCurrent,
+        "interview": interview,
+        "warNews_1": warNews_1,
+        "warNews": warNews
     }
+    print(currentNews)
     return render(request, 'home.html', ctx)
 
 
